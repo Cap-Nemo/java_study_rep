cc036d06c0e083b21fb293ca01cc3a7d7aa623cf
eivonin <e.ivonin@hh.ru>
2016-06-07 15:07:22 +0300

    Создал базовый класс, добавил тест удаления групп, вынес вспомогательные методы в базовый класс

---
+++ addressbook-web-tests/src/test/java/ru/testpack/adressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.testpack.adressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
